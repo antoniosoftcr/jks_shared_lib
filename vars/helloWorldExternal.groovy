ce761e342a6ae7e0778379adbdda14cbc5abe706
antoniom <19955241+antoniosoftcr@users.noreply.github.com>
2025-07-18 14:06:25 -0600

    fixed params

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,6 +1,6 @@
 def call(Map params = [:]) {
     loadLinuxScript(name: 'hello-world.sh')
     loadLinuxScriptMyFolder(name: 'hello-world-mf.sh')
-    sh "./hello-world.sh ${params.name} ${config.dayOfWeek}"
-    sh "./hello-worldmf.sh ${params.name} ${config.dayOfWeek}"
+    sh "./hello-world.sh ${params.name} ${params.dayOfWeek}"
+    sh "./hello-worldmf.sh ${params.name} ${params.dayOfWeek}"
 }
